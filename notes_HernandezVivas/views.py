97c1a9cfa6032da9ada1eb54da1557b71d041558
MacYHD <mac@MacBook-Air-de-YHR.local>
2024-10-22 04:26:28 -0600

    se agrego diseno a la lista con boostrap

--- notes_HernandezVivas/views.py
+++ notes_HernandezVivas/views.py
@@ -29,7 +29,6 @@ class CreateNotaView(LoginRequiredMixin, CreateView):
     fields = ['title', 'content']
     success_url = reverse_lazy('home')
 
-
     def form_valid(self, form):
         form.instance.user = self.request.user  
         return super().form_valid(form)
